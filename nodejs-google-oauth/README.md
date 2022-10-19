0a413154957a18abc80c538a50627fc71fe4d00e
Shanil Jasani <shaniljasani@utexas.edu>
2022-10-19 16:49:38 -0500

    + Google OAuth Sandbox

---
+++ nodejs-google-oauth/README.md
@@ -0,0 +1,5 @@
+# Google OAuth with Node.js
+
+I was working on a project and needed a sandbox to figure out the best way to use Google Accounts as logins for a web app.
+
+Special thanks to [LoginRadius](https://www.loginradius.com/blog/engineering/google-authentication-with-nodejs-and-passportjs/)
---
+++ nodejs-google-oauth/index.js
@@ -0,0 +1,78 @@
+// index.js
+
+/**
+ * Load environment variables from .env file, where API keys and passwords are configured.
+ */
+ const dotenv = require('dotenv');
+ dotenv.config({ path: '.env' });
+
+/*  EXPRESS */
+
+const express = require('express');
+const app = express();
+const session = require('express-session');
+
+app.set('view engine', 'ejs');
+
+app.use(session({
+  resave: false,
+  saveUninitialized: true,
+  secret: 'SECRET' 
+}));
+
+app.get('/', function(req, res) {
+  res.render('pages/auth');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port , () => console.log('App listening on port ' + port));
+
+/*  PASSPORT SETUP  */
+
+const passport = require('passport');
+var userProfile;
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set('view engine', 'ejs');
+
+app.get('/success', (req, res) => {
+  res.render('pages/success', {user: userProfile});
+  // console.log(userProfile);
+});
+app.get('/error', (req, res) => res.send("error logging in"));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+/*  Google AUTH  */
+ 
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GOOGLE_CLIENT_ID = 'our-google-client-id';
+const GOOGLE_CLIENT_SECRET = 'our-google-client-secret';
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+      userProfile=profile;
+      return done(null, userProfile);
+  }
+));
+ 
+app.get('/auth/google', 
+  passport.authenticate('google', { scope : ['profile', 'email'] }));
+ 
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/error' }),
+  function(req, res) {
+    // Successful authentication, redirect success.
+    res.redirect('/success');
+  });
